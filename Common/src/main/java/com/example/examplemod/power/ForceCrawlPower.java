0399f796f7101e76267fe2d41ce7189dc8088f46
Pug <batterypug@gmail.com>
2022-08-24 19:47:21 +1000

    Update build scripts further, add registration stuff, start on power

--- Common/src/main/java/com/example/examplemod/power/ForceCrawlPower.java
+++ Common/src/main/java/com/example/examplemod/power/ForceCrawlPower.java
@@ -1,4 +1,11 @@
 package com.example.examplemod.power;
 
+import io.github.apace100.apoli.power.Power;
+import io.github.apace100.apoli.power.PowerType;
+import net.minecraft.world.entity.LivingEntity;
+
 public class ForceCrawlPower extends Power {
+    public ForceCrawlPower(PowerType<?> type, LivingEntity entity) {
+        super(type, entity);
+    }
 }
--- Fabric/src/main/java/com/example/examplemod/ExampleMod.java
+++ Fabric/src/main/java/com/example/examplemod/ExampleMod.java
@@ -14,5 +14,7 @@ public class ExampleMod implements ModInitializer {
         // Use Fabric to bootstrap the Common mod.
         Constants.LOG.info("Hello Fabric world!");
         CommonClass.init();
+
+
     }
 }
